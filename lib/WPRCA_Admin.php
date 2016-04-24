4b96fa3e224fb00a6ecf385028f50348234d66e2
Alexandra Nyitraiova <alexandranyitraiova@gmail.com>
2016-04-24 15:48:38 +0200

    Create basic html/css setup page, for plugin setup

--- lib/WPRCA_Admin.php
+++ lib/WPRCA_Admin.php
@@ -28,7 +28,41 @@ class WPRCA_Admin {
 			}
 
 			echo '<div class="wrap">';
+			echo '<h1> Nastavenie revízii</h1>';
 			echo '<p>Here is where the form would go if I actually had options.</p>';
+			echo '<div class="one-half">
+						<div><p>Prehľad</p></div>
+						<div>
+							<div class="progress">
+								<p class="progress__info align_text"> Čas do ďalšieho čistenia</p>
+								<p class="progress__timer align_text">00:30</p> 
+								<p class="progress__info align_text">Počet čistení: 2000</p>
+								<p class="progress__info align_text">Vzmazané revízie: 380000</p>
+								<input class="progress__info__button" type="submit" name="delete_revisions" value="Delete revisions">
+							</div>
+						</div>
+
+						
+					</div>
+
+					<div class="one-half">
+						<div><p>Nastavenie</p></div>
+						<form method="post">
+							<div class="range_input some_padding">
+								<label class="range_input__info" for="weight">Počet uchovávaných dní</label>
+								<input class="range_input__info" type="range" id="weight" min="10" value="10" max="2000" step="100">
+								<input class="range_input__info" type="text" name="">
+							</div>
+							<div class="range_input some_padding">
+								<label class="range_input__info" for="weight">Počet revízii</label>
+								<input class="range_input__info" type="range" id="weight" min="10" value="10" max="2000" step="100">
+								<input class="range_input__info" type="text" name="">		
+							</div>				
+							<div>
+								<input class="progress__info__button" type="submit" name="delete_revisions" value="Uložiť">
+							</div>
+						</form>
+					</div>';
 			echo '</div>';
 
     }
@@ -37,7 +71,7 @@ class WPRCA_Admin {
 
     	wp_enqueue_script( 'wprca-script' , plugins_url( 'assets/ajax.js', WPRCA_PLUGIN_INDEX ), array("jquery"), '0.0.1', 'true' );
 
-    	wp_enqueue_style( 'style', plugins_url( 'assets/style.css', WPRCA_PLUGIN_INDEX), array(""), "0.0.1", "all" );
+    	wp_enqueue_style( 'style', plugins_url( 'assets/style.min.css', WPRCA_PLUGIN_INDEX), array(), "0.0.1", "all" );
 
     }
 
