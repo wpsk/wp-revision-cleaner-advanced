25121696c505456f626889efa8566805ccb9cf81
jurajk <juraj.kiss@gmail.com>
2016-04-24 12:08:20 +0200

    replace spaces with tabs

--- lib/WPRCA_Core.php
+++ lib/WPRCA_Core.php
@@ -2,15 +2,15 @@
 
 /**
  * Core class of the plugin that bootstraps all the functionality.
- * 
+ *
  * @since 1.0.0
  * @author Martin Krcho <martin@mojandroid.sk>
  */
 class WPRCA_Core {
 
-    public static function init() {
-	
-	//  add calls to other init functions here.
-    }
+		public static function init() {
+
+	//	add calls to other init functions here.
+		}
 
 }
