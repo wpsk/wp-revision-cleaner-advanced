f14220ce86c70f2329bf95250bd72375fc3cc689
Martin Krcho <Martin Krcho@MartinKrcho-PC>
2016-04-24 11:40:01 +0200

    Added basic building blocks of the plugin.

---
+++ lib/WPRCA_Core.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Core class of the plugin that bootstraps all the functionality.
+ * 
+ * @since 1.0.0
+ * @author Martin Krcho <martin@mojandroid.sk>
+ */
+class WPRCA_Core {
+
+    public static function init() {
+	
+	//  add calls to other init functions here.
+    }
+
+}
